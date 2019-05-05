6aed6ebf2dd36b4b8f9ec6f682e05df6b3e7af0d
Nate Bennett <26212562+NateBennett9@users.noreply.github.com>
2019-05-04 21:08:10 -0700

    final updated

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,42 +1,67 @@
-$(document).ready(function () {
-    var topics = ["LeBron James", "Kevin Durant", "Anthony Davis", "James Harden", "Stephen Curry", "Giannis Antetokounmpo", "Joel Embiid", "Russell Westbrook", "Kawhi Leonard"];
-    var apiKey = "https://api.giphy.com/v1/gifs/random?api_key=9CP1fq0g6nRVwtPMxybIedfL6FzoD7qF";
-    var buttonNum = 0;
+$(function() {
+    createButtons(topics, 'searchButton', '#buttonsArea');
+    console.log('Page Loaded');
+})
 
-    for (var i = 0; i < topics.length; i++) {
-        //$(".buttonList").append("<button class='btn btn-primary'>" + topics[i] + "</button>");
-        $(".buttonList").append("<button class='btn btn-primary' id='button" + buttonNum + "'>" + topics[i] + "</button>");
-        buttonNum++;
-        console.log($("#button" + i).text());
+    var topics = ["LeBron James", "Kevin Durant", "Anthony Davis", "James Harden", "Stephen Curry", "Giannis Antetokounmpo", "Joel Embiid", "Russell Westbrook"];
+
+    function createButtons(topics, newClass, newDiv) {
+        $(newDiv).empty();
+        for (var i = 0; i < topics.length; i++) {
+            var a = $('<button>');
+            a.addClass(newClass);
+            a.attr('data-type', topics[i]);
+            a.text(topics[i]);
+            $(newDiv).append(a);
+        }
     }
 
-    $("button").on("click", function(){
-        /*
+    $(document).on('click','.searchButton',function() {
+        $('#searches').empty();
+        var type = $(this).data('type');
+        console.log(type);
+        var queryURL = "http://api.giphy.com/v1/gifs/search?q="+type+"&api_key=9CP1fq0g6nRVwtPMxybIedfL6FzoD7qF&limit=10";
         $.ajax({
-            url: apiKey + "&tag=" + $(this).text(),
-            method: 'GET'
-        }).then(function(response){
-            console.log($(this).text());
-            console.log(response);
-            $(".container").prepend("<img class='gif' src='" + response.data.image_original_url + "'>")
-            $("img").attr("alt", $(this).text + " image");
-        });
-        */ 
-        //console.log($(this).text());
-        for(var i = 0; i < 10; i++){
-            $.ajax({
-                url: apiKey + "&tag=" + $(this).text(),
-                method: 'GET'
-            }).then(function(response){
-                $(".container").prepend("<img class='gif' id='gif" + i + "' src='" + response.data.image_original_url + "'>");
-                $("img").attr("alt", $(this).text() + " image");
-            });
+            url:queryURL,
+            method:'GET'
+        })
+            .done(function(res) {
+                console.log(res);
+                for (var i = 0; i < res.data.length; i++) {
+                    var searchDiv = $('<div class="search-item">');
+                    var rating = res.data[i].rating;
+                    var p = $('<p>').text('Rating: ' + rating);
+                    var animated = res.data[i].images.fixed_height.url;
+                    var still = res.data[i].images.fixed_height_still.url;
+                    var image = $('<img>');
+                    image.attr('src', still);
+                    image.attr('data-still', still);
+                    image.attr('data-animated', animated);
+                    image.attr('data-state', 'still');
+                    image.addClass('searchImage');
+                    searchDiv.append(p);
+                    searchDiv.append(image);
+                    $('#searches').append(searchDiv);
+                }
+            })
+    })
+
+    $(document).on('click', '.searchImage', function () {
+        var state = $(this).attr('data-state');
+        if (state == 'still') {
+            $(this).attr('src', $(this).data('animated'));
+            $(this).attr('data-state', 'animated');
         }
-    });
+        else
+        {
+            $(this).attr('src', $(this).data('still'));
+            $(this).attr('data-state', 'still');
+        }
+    })
 
-    $("aside").append("<button class='clear'>CLEAR</button>");
-    $(".clear").on("click", function(){
-        $(".container").empty();
-    });
-});
-
+$('#addSearch').on('click', function() {
+    var newSearch = $('input').eq(0).val();
+    topics.push(newSearch);
+    createButtons(topics, 'searchButton', '#buttonsArea');
+    return false;
+})
